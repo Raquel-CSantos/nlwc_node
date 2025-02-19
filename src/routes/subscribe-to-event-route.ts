b8018a26954c154f676d1d6c91c2eadee19240e1
raquel.santos <raquel.santos@grupomoura.com>
2025-02-19 17:24:27 -0300

    add first route and swagger

---
+++ src/routes/subscribe-to-event-route.ts
@@ -0,0 +1,33 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: {
+          201: z.object({
+            name: z.string(),
+            email: z.string(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { name, email } = req.body
+
+      //criação da inscrição no banco de dados
+
+      return res.status(201).send({
+        name,
+        email,
+      })
+      // 201 -> foi sucesso e um recurso foi criado!
+    }
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import Fastify from 'fastify'
 import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 
-const app = Fastify()
+import { z } from 'zod'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+
+const app = Fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -15,9 +22,21 @@ app.register(fastifyCors)
 //   origin: ''
 // })
 
-app.get('/', () => {
-  return 'hello world'
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
